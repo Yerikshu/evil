4e630d062dcf565c2473e346e561b476a75ed2ab
yerikyu <19802021029@139.com>
2020-03-17 11:48:05 +0800

    update view

--- evil/urls.py
+++ evil/urls.py
@@ -14,10 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from django.contrib import admin
-from django.urls import path
 
-from evilvisitor.views import VisitorView
+from evilvisitor.views.VisitorViews import VisitorView
 
 urlpatterns = [
     url(r'^evil', VisitorView.as_view(), name='evil'),
